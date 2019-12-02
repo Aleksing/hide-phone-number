342b01c728fb97d0b4b94cd2d6fd19ebc3f4d0ba
aleksandr05 <48736594+aleksandr05@users.noreply.github.com>
2019-12-02 23:32:53 +0300

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,4 @@
+document.querySelector('#ntel').textContent = '+7 909 ххх-показать';
+document.querySelector('#ntel').onmouseover = function (){
+    document.querySelector('#ntel').textContent = '+7 909 214-49-50';
+};
